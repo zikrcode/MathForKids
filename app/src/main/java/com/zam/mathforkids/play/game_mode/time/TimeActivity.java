7653334f7bd41dd0997ab3a9826379c915a70322
Zokirjon Mamadjonov <zmamadjonov@freeportmetrics.com>
2023-06-14 20:47:07 +0200

    updated time mode

--- app/src/main/java/com/zam/mathforkids/play/game_mode/time/TimeActivity.java
+++ app/src/main/java/com/zam/mathforkids/play/game_mode/time/TimeActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.MotionEvent;
@@ -18,39 +17,25 @@ import com.zam.mathforkids.play.game_mode.result.ResultActivity;
 
 public class TimeActivity extends AppCompatActivity implements View.OnTouchListener{
 
-    private SharedPreferences sharedPreferences1, sharedPreferences2;
-    private MyTimer myTimer;
-    private int n=0, ansCorrect=0, c, max, min, ans;
-    private  String s, question;
-    private Question[] timeQuestions;
+    private SharedPreferences sharedPreferencesDigitRange, sharedPreferencesOperation;
     private ImageView ivBackTA, ivATA, ivBTA, ivCTA, ivDTA;
     private TextView tvNumberOfQuestionTA, tvTimerTA, tvXTA, tvOperationTA, tvYTA, tvATA, tvBTA, tvCTA, tvDTA;
-    private String correct="", wrong="";
+    private MyTimer myTimer;
+    private int n = 0, ansCorrect = 0, c, max, min, ans;
+    private  String operation, question;
+    private Question[] timeQuestions;
+    private String correct = "", wrong = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_time);
 
-        sharedPreferences1 = getApplicationContext().getSharedPreferences("DIGIT_RANGE",MODE_PRIVATE);
-        sharedPreferences2 = getApplicationContext().getSharedPreferences("OPERATION",MODE_PRIVATE);
-
-        myTimer =  new MyTimer(sharedPreferences1.getInt("SECONDS",10000),1000);
-        min = sharedPreferences1.getInt("MIN",10);
-        max = sharedPreferences1.getInt("MAX",20);
-        s = sharedPreferences2.getString("Operation","+");
-
-        timeQuestions = new Question[10];
-        for (int i=0; i<10; i++){
-            timeQuestions[i] = new Question(s,max,min);
-
-        }
-
-        tvTimerTA = findViewById(R.id.tvTimerTA);
+        sharedPreferencesDigitRange = getApplicationContext().getSharedPreferences("DIGIT_RANGE" ,MODE_PRIVATE);
+        sharedPreferencesOperation = getApplicationContext().getSharedPreferences("OPERATION", MODE_PRIVATE);
 
         ivBackTA = findViewById(R.id.ivBackTA);
-        ivBackTA.setOnTouchListener(this);
+        tvTimerTA = findViewById(R.id.tvTimerTA);
 
         tvNumberOfQuestionTA = findViewById(R.id.tvNumberOfQuestionTA);
 
@@ -58,32 +43,48 @@ public class TimeActivity extends AppCompatActivity implements View.OnTouchListe
         tvOperationTA = findViewById(R.id.tvOperationTA);
         tvYTA = findViewById(R.id.tvYTA);
 
-        switch (s){
-            case "+": tvOperationTA.setText("+"); break;
-            case "-": tvOperationTA.setText("-"); break;
-            case "×": tvOperationTA.setText("×"); break;
-            case "÷": tvOperationTA.setText("÷"); break;
-        }
-
         tvATA = findViewById(R.id.tvATA);
         tvBTA = findViewById(R.id.tvBTA);
         tvCTA = findViewById(R.id.tvCTA);
         tvDTA = findViewById(R.id.tvDTA);
 
         ivATA = findViewById(R.id.ivATA);
-        ivATA.setOnTouchListener(this);
         ivBTA = findViewById(R.id.ivBTA);
-        ivBTA.setOnTouchListener(this);
         ivCTA = findViewById(R.id.ivCTA);
-        ivCTA.setOnTouchListener(this);
         ivDTA = findViewById(R.id.ivDTA);
+
+        setupViews();
+    }
+
+    private void setupViews() {
+        myTimer =  new MyTimer(sharedPreferencesDigitRange.getInt("SECONDS",10000),1000);
+        min = sharedPreferencesDigitRange.getInt("MIN",10);
+        max = sharedPreferencesDigitRange.getInt("MAX",20);
+        operation = sharedPreferencesOperation.getString("Operation","+");
+
+        timeQuestions = new Question[10];
+        for (int i = 0; i < 10; i++) {
+            timeQuestions[i] = new Question(operation, max, min);
+        }
+
+        switch (operation) {
+            case "+": tvOperationTA.setText("+"); break;
+            case "-": tvOperationTA.setText("-"); break;
+            case "×": tvOperationTA.setText("×"); break;
+            case "÷": tvOperationTA.setText("÷"); break;
+        }
+
+        ivBackTA.setOnTouchListener(this);
+        ivATA.setOnTouchListener(this);
+        ivBTA.setOnTouchListener(this);
+        ivCTA.setOnTouchListener(this);
         ivDTA.setOnTouchListener(this);
 
         setUpQuestion();
     }
 
     private void setUpQuestion() {
-        tvNumberOfQuestionTA.setText(String.valueOf(n+1)+"/10");
+        tvNumberOfQuestionTA.setText((n + 1) + "/10");
 
         myTimer.start();
 
@@ -99,11 +100,11 @@ public class TimeActivity extends AppCompatActivity implements View.OnTouchListe
         tvCTA.setText(String.valueOf(random[2]));
         tvDTA.setText(String.valueOf(random[3]));
 
-        switch ((int) (Math.random() * 5)){
-            case 1: ans=ivATA.getId(); tvATA.setText(String.valueOf(c)); break;
-            case 2: ans=ivBTA.getId(); tvBTA.setText(String.valueOf(c)); break;
-            case 3: ans=ivCTA.getId(); tvCTA.setText(String.valueOf(c)); break;
-            default: ans=ivDTA.getId(); tvDTA.setText(String.valueOf(c)); break;
+        switch ((int) (Math.random() * 5)) {
+            case 1: ans = ivATA.getId(); tvATA.setText(String.valueOf(c)); break;
+            case 2: ans = ivBTA.getId(); tvBTA.setText(String.valueOf(c)); break;
+            case 3: ans = ivCTA.getId(); tvCTA.setText(String.valueOf(c)); break;
+            default: ans = ivDTA.getId(); tvDTA.setText(String.valueOf(c)); break;
         }
     }
 
@@ -111,42 +112,46 @@ public class TimeActivity extends AppCompatActivity implements View.OnTouchListe
     public boolean onTouch(View v, MotionEvent event) {
         ImageView imageView = (ImageView) v;
 
-        if(event.getAction() == MotionEvent.ACTION_DOWN){
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
             imageView.setColorFilter(getColor(R.color.transparent));
         }
-        if(event.getAction() == MotionEvent.ACTION_UP){
+        if (event.getAction() == MotionEvent.ACTION_UP) {
             imageView.clearColorFilter();
 
             myTimer.cancel();
 
-            if (imageView.getId()==R.id.ivBackTA){
+            if (imageView.getId() == R.id.ivBackTA) {
                 finish();
             }
             else {
                 n++;
-                if (imageView.getId()==ans){
+                if (imageView.getId() == ans) {
                     ansCorrect++;
-                    correct+=question+"\n";
+                    correct += question + "\n";
                 }
                 else {
                     changeAns(imageView.getId());
-                    wrong+=question+"\n";
+                    wrong += question + "\n";
                 }
-                if (n==10) {finishResult();}
-                else {setUpQuestion();}
+                if (n == 10) {
+                    finishResult();
+                }
+                else {
+                    setUpQuestion();
+                }
             }
         }
         return true;
     }
 
     private void changeAns(int id) {
-        int x=question.indexOf('=')+1;
+        int x = question.indexOf('=') + 1;
 
         switch (id){
-            case R.id.ivATA: question=question.substring(0,x)+tvATA.getText(); break;
-            case R.id.ivBTA: question=question.substring(0,x)+tvBTA.getText(); break;
-            case R.id.ivCTA: question=question.substring(0,x)+tvCTA.getText(); break;
-            case R.id.ivDTA: question=question.substring(0,x)+tvDTA.getText(); break;
+            case R.id.ivATA: question = question.substring(0, x) + tvATA.getText(); break;
+            case R.id.ivBTA: question = question.substring(0, x) + tvBTA.getText(); break;
+            case R.id.ivCTA: question = question.substring(0, x) + tvCTA.getText(); break;
+            case R.id.ivDTA: question = question.substring(0, x) + tvDTA.getText(); break;
         }
     }
 
@@ -154,7 +159,7 @@ public class TimeActivity extends AppCompatActivity implements View.OnTouchListe
         Intent intent = new Intent(TimeActivity.this, ResultActivity.class);
         intent.putExtra("ACTIVITY","TA");
         intent.putExtra("CORRECT",String.valueOf(ansCorrect));
-        intent.putExtra("WRONG",String.valueOf(10-ansCorrect));
+        intent.putExtra("WRONG",String.valueOf(10 - ansCorrect));
         intent.putExtra("C_QUESTIONS",correct);
         intent.putExtra("W_QUESTIONS",wrong);
         startActivity(intent);
@@ -167,7 +172,7 @@ public class TimeActivity extends AppCompatActivity implements View.OnTouchListe
         finish();
     }
 
-    private class MyTimer extends CountDownTimer{
+    private class MyTimer extends CountDownTimer {
 
         public MyTimer(long millisInFuture, long countDownInterval) {
             super(millisInFuture, countDownInterval);
@@ -181,9 +186,13 @@ public class TimeActivity extends AppCompatActivity implements View.OnTouchListe
         @Override
         public void onFinish() {
             n++;
-            wrong+=getString(R.string.not_solved)+"\n";
-            if (n==10) {finishResult();}
-            else {setUpQuestion();}
+            wrong += getString(R.string.not_solved) + "\n";
+            if (n == 10) {
+                finishResult();
+            }
+            else {
+                setUpQuestion();
+            }
         }
     }
 }
